108561eea5e3e112d8d1ba22cf2836bd2e7af711
Naomi Koh <nck23@cornell.edu>
2020-04-01 17:22:41 -0400

    Updates

---
+++ precache-manifest.78a6e94a2be0fe8d507515b2fba78e61.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba6a95f89d8a4fb6cdb90507cdfa7c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "536c4b509940817c9687",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2698791d002ec9a81914",
+    "url": "/static/css/main.1f3c9bdf.chunk.css"
+  },
+  {
+    "revision": "536c4b509940817c9687",
+    "url": "/static/js/2.1107f020.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.1107f020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2698791d002ec9a81914",
+    "url": "/static/js/main.998650b1.chunk.js"
+  },
+  {
+    "revision": "707c8f0cbad1ba39d05e",
+    "url": "/static/js/runtime-main.90a1d854.js"
+  },
+  {
+    "revision": "687d79b6890035957cdeacc3ebdaf261",
+    "url": "/static/media/aliens.687d79b6.png"
+  },
+  {
+    "revision": "810a0590aea90a8709aedb9d484d4495",
+    "url": "/static/media/beach.810a0590.jpg"
+  },
+  {
+    "revision": "5a23d7082ad5539415bb3d6314cf85ec",
+    "url": "/static/media/bench.5a23d708.png"
+  },
+  {
+    "revision": "5922fa6e08d591c32f108bddb64431a2",
+    "url": "/static/media/boardwalk.5922fa6e.png"
+  },
+  {
+    "revision": "ad9c5de9ecf84a7a836d9db63508dce8",
+    "url": "/static/media/boulder.ad9c5de9.png"
+  },
+  {
+    "revision": "1b010b2ffdafe1567a869902d225d610",
+    "url": "/static/media/bryant.1b010b2f.png"
+  },
+  {
+    "revision": "cb58f74439bc21843a8388512044ce67",
+    "url": "/static/media/cancel.cb58f744.png"
+  },
+  {
+    "revision": "bc91702d778e27aa3a171a70929d7432",
+    "url": "/static/media/car.bc91702d.png"
+  },
+  {
+    "revision": "c4174e49179c9676a6654fdf87da1d11",
+    "url": "/static/media/car.c4174e49.png"
+  },
+  {
+    "revision": "58c6f57fdcc8e6b5b7eeaca792d6c5a5",
+    "url": "/static/media/claire.58c6f57f.m4a"
+  },
+  {
+    "revision": "a1826657789e9e2108f6bfcd7ade322c",
+    "url": "/static/media/coney.a1826657.png"
+  },
+  {
+    "revision": "913a2b4ba357a4737ef357dbd22d0ab8",
+    "url": "/static/media/crabs.913a2b4b.png"
+  },
+  {
+    "revision": "c5389bff667098fd736d034806d63c39",
+    "url": "/static/media/danny.c5389bff.png"
+  },
+  {
+    "revision": "74012d707ba9fc6e09951dace7de9659",
+    "url": "/static/media/deepfried.74012d70.png"
+  },
+  {
+    "revision": "e060fd01b89bf1dc04e85768a19c2689",
+    "url": "/static/media/doll.e060fd01.png"
+  },
+  {
+    "revision": "3e9016dc634aa80566c2ef449122b4b8",
+    "url": "/static/media/draw.3e9016dc.png"
+  },
+  {
+    "revision": "088dc4d13b4309e51ac40a709e3e9faa",
+    "url": "/static/media/dumbo.088dc4d1.png"
+  },
+  {
+    "revision": "b47ca8b93ee01293e1b9cccec49b9a88",
+    "url": "/static/media/eyebrows.b47ca8b9.png"
+  },
+  {
+    "revision": "088bda894b6ec1511804c8ef92073717",
+    "url": "/static/media/facetime.088bda89.png"
+  },
+  {
+    "revision": "b858bcb6a700a439c62f35b517edccc7",
+    "url": "/static/media/game.b858bcb6.png"
+  },
+  {
+    "revision": "3e0c62af440ca32071d7131908ab5c4f",
+    "url": "/static/media/goldy.3e0c62af.png"
+  },
+  {
+    "revision": "e42cfce1cafcdcaf5da886442762cd6c",
+    "url": "/static/media/grass.e42cfce1.png"
+  },
+  {
+    "revision": "331948280ad7c3ee01edba0282d60f04",
+    "url": "/static/media/header.33194828.jpg"
+  },
+  {
+    "revision": "2c6b47e02c0a11cd57bf0be1baf93879",
+    "url": "/static/media/krakatoa.2c6b47e0.png"
+  },
+  {
+    "revision": "69d0bd345d6a1198ca7910d5099c251e",
+    "url": "/static/media/lego.69d0bd34.png"
+  },
+  {
+    "revision": "168b9774f97d5fa6a0651f08f1f008d9",
+    "url": "/static/media/lord.168b9774.png"
+  },
+  {
+    "revision": "daaebbcfa854eb573f7f349f3e59593c",
+    "url": "/static/media/manav.daaebbcf.png"
+  },
+  {
+    "revision": "881c50ec7e7b2b161dcd3afed1acfcb1",
+    "url": "/static/media/meme.881c50ec.png"
+  },
+  {
+    "revision": "a13b5cf5f685f14573c3d8ab85994dce",
+    "url": "/static/media/mushroom.a13b5cf5.png"
+  },
+  {
+    "revision": "43221b823e1f718b01d3c9469e10af6d",
+    "url": "/static/media/nowhere.43221b82.png"
+  },
+  {
+    "revision": "20aa510063a2416d02aeec6b0eff6497",
+    "url": "/static/media/nowhereAgain.20aa5100.png"
+  },
+  {
+    "revision": "6af7cae5a0918b02fc699258199d1671",
+    "url": "/static/media/nyc.6af7cae5.png"
+  },
+  {
+    "revision": "b0b5b7e5604a939332fdc19dbc6fab73",
+    "url": "/static/media/pret.b0b5b7e5.png"
+  },
+  {
+    "revision": "bd4a47e61e1885be9a41c34770c1077a",
+    "url": "/static/media/prom.bd4a47e6.jpg"
+  },
+  {
+    "revision": "a3b0c94e3e71fe645aa0e371f961fcd8",
+    "url": "/static/media/prom2.a3b0c94e.jpg"
+  },
+  {
+    "revision": "69bde266842e357a11de879501a070b7",
+    "url": "/static/media/ralphs.69bde266.png"
+  },
+  {
+    "revision": "ded00ab85e808fa9790efead4bc8e81d",
+    "url": "/static/media/reg.ded00ab8.png"
+  },
+  {
+    "revision": "ec1413b15066d09c41509406bf260af4",
+    "url": "/static/media/sephora.ec1413b1.png"
+  },
+  {
+    "revision": "ed3f4d88205a220ec5d68d603db6e89a",
+    "url": "/static/media/sleep.ed3f4d88.png"
+  },
+  {
+    "revision": "b076e257b11c27b5ce236763ef869608",
+    "url": "/static/media/spongebob.b076e257.png"
+  },
+  {
+    "revision": "0508a6bc41c889e1cd78fdfc19ff22e3",
+    "url": "/static/media/statue.0508a6bc.png"
+  },
+  {
+    "revision": "9f2fd87b8ee510db0baf57669566bb74",
+    "url": "/static/media/student.9f2fd87b.png"
+  },
+  {
+    "revision": "d551e914f5679ce4fcf9576d9a592371",
+    "url": "/static/media/subway.d551e914.png"
+  },
+  {
+    "revision": "3af683cc67bdae65ec9a8783f46ea9a8",
+    "url": "/static/media/taylor.3af683cc.png"
+  },
+  {
+    "revision": "adcae8d62f39bcc28815028c481a5335",
+    "url": "/static/media/theMet.adcae8d6.png"
+  },
+  {
+    "revision": "ac01c9736cd55bff501aa9f30df6f8fc",
+    "url": "/static/media/wizard.ac01c973.png"
+  }
+]);
